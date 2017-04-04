8e1edad87c42b8f94509871c7c875ffc1debaad7
gamer09cod <gamer09cod@gmail.com>
2017-04-04 16:29:52 +0530

    Add files via upload
    First Commit

---
+++ Scripts/DestroyByContact.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	private GameController gameController;//reffering gameController from this class to add scoreValue
+	public int scoreValue;
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) 
+		{
+			gameController = gameControllerObject.GetComponent<GameController> ();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("No Game 'Controller Script' found");
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boundary") {
+			return;
+		}
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}	
+		gameController.AddScore (scoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
---
+++ Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public float tumble;
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	void Update () {
+		
+	}
+}
